ff134c508944ac5b1003a9f08e85dfc286f00e16
Robin Portigliatti <robin.portigliatti@dalibo.com>
2023-06-05 10:40:07 +0200

    Adding optimisation to field functions

--- airtable/field.go
+++ airtable/field.go
@@ -4,32 +4,22 @@ import (
 	"github.com/mehanizm/airtable"
 )
 
-// IsOneToMany checks if the field represents a OneToMany relationship
+// prefersSingleRecordLink is a helper function that gets the value of "prefersSingleRecordLink" from the field options.
+func prefersSingleRecordLink(f *airtable.Field) bool {
+	return f.Options["prefersSingleRecordLink"].(bool)
+}
+
+// IsOneToMany checks if the field represents a OneToMany relationship.
 func IsOneToMany(f *airtable.Field, field *airtable.Field) bool {
-	fPrefersSingleRecordLink := f.Options["prefersSingleRecordLink"].(bool)
-	fieldPrefersSingleRecordLink := field.Options["prefersSingleRecordLink"].(bool)
-	if !fPrefersSingleRecordLink && fieldPrefersSingleRecordLink{
-		return true
-	}
-	return false
+	return !prefersSingleRecordLink(f) && prefersSingleRecordLink(field)
 }
 
-// IsManyToOne checks if the field represents a ManyToOne relationship
+// IsManyToOne checks if the field represents a ManyToOne relationship.
 func IsManyToOne(f *airtable.Field, field *airtable.Field) bool {
-	fPrefersSingleRecordLink := f.Options["prefersSingleRecordLink"].(bool)
-	fieldPrefersSingleRecordLink := field.Options["prefersSingleRecordLink"].(bool)
-	if fPrefersSingleRecordLink && !fieldPrefersSingleRecordLink{
-		return true
-	}
-	return false
+	return prefersSingleRecordLink(f) && !prefersSingleRecordLink(field)
 }
 
-// IsManyToMany checks if the field represents a ManyToMany relationship
+// IsManyToMany checks if the field represents a ManyToMany relationship.
 func IsManyToMany(f *airtable.Field, field *airtable.Field) bool {
-	fPrefersSingleRecordLink := f.Options["prefersSingleRecordLink"].(bool)
-	fieldPrefersSingleRecordLink := field.Options["prefersSingleRecordLink"].(bool)
-	if !fPrefersSingleRecordLink && !fieldPrefersSingleRecordLink{
-		return true
-	}
-	return false
+	return !prefersSingleRecordLink(f) && !prefersSingleRecordLink(field)
 }
