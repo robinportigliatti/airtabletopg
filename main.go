8119915dde01c5860a0a026965fcc76d156a8b15
Robin Portigliatti <robin.portigliatti@dalibo.com>
2023-06-05 11:24:51 +0200

    optimizing exporttable to csv

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	a "airtabletopg/airtable"
 	"airtabletopg/postgres"
 	"encoding/csv"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -14,6 +15,9 @@ import (
 
 func exportTableToCSV(client *airtable.Client, b *airtable.Base, r []*a.RelationshipInfo, tableName string) error {
 //	fmt.Printf("Processing table: %s\n", tableName)
+	// Get schema and current table
+	schema, err := client.GetBaseSchema(b.ID).Do()
+	currentTable, _ := a.FindTableByName(schema.Tables, tableName)
 	table := client.GetTable(b.ID, tableName)
 	records,_ := table.GetRecords().Do()
 
@@ -28,77 +32,100 @@ func exportTableToCSV(client *airtable.Client, b *airtable.Base, r []*a.Relation
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	schema, _ := client.GetBaseSchema(b.ID).Do()
-	currentTable, _ := a.FindTableByName(schema.Tables, tableName)
-
 	for _, record := range records.Records {
-		var row []string
-		var keys []string
-		for k := range record.Fields {
-			keys = append(keys, k)
+		row, err := processRecord(record, currentTable, r, client, b)
+		if err != nil {
+			return err
 		}
-		sort.Strings(keys)
-		
-		for _, fieldName := range keys {
-			fieldValue := record.Fields[fieldName]
-			var appendBool = true
-			var valueToStore = fieldValue
-			// Si le duo fieldName
-			// Dans notre objet Base on a notre table courante et le field courant
-			currentField, _ := a.FindFieldByName(currentTable.Fields, fieldName)
-			if (currentField.Type == "multipleRecordLinks") {
-				currentRelationIfFound, _ := a.FindRelationByTableIDAndFieldID(r, currentTable.ID, currentField.ID)
-				if currentRelationIfFound != nil {
-					if currentRelationIfFound.RelationType == "OneToMany" {
-						appendBool = false
-					}
-					if currentRelationIfFound.RelationType == "ManyToMany" {
-						appendBool = false
-					}
-					if currentRelationIfFound.RelationType == "ManyToOne" {
-						// Ici on récupère en fonction de la valeur le record de la table liée et du champs lié en fonction de l'identifiant
-						// unique présent dans fieldValue
-						//fmt.Println(fieldValue)
-						fieldValueSlice, ok := fieldValue.([]interface{})
-						if !ok {
-							fmt.Println("fieldValue n'est pas un slice")
-							continue
-						}
-						id, ok := fieldValueSlice[0].(string)
-						var relatedTable =  client.GetTable(b.ID, currentRelationIfFound.RelatedTable.Name)
-						currentRecord, _ := relatedTable.GetRecord(id)
-						primaryField, _ := a.FindFieldByID(&currentRelationIfFound.RelatedTable, currentRelationIfFound.RelatedTable.PrimaryFieldID)
-						appendBool = false
-						valueToStore = currentRecord.Fields[primaryField.Name]
-					}
-				}
-			}
-			// fmt.Println(currentField)
-			if appendBool {
-				row = append(row, fmt.Sprintf("%v", valueToStore))
-			}
-		}
-	
+
 		err = writer.Write(row)
 		if err != nil {
 			return err
 		}
 	}
-	// FIXME: A la fin on écrit dans data.sql le COPY
-	var keys []string
-	record := records.Records[0]
-	for k := range record.Fields {
-		keys = append(keys, k)
-		//fmt.Println(k)
+	fmt.Println(len(records.Records))
+	if len(records.Records) != 0 {
+
+		var keys []string
+		record := records.Records[0]
+		for k := range record.Fields {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		var sql []string
+		sql = append(sql, fmt.Sprintf("\\COPY %s (" + strings.Join(keys, ", ") + ") FROM '%s' DELIMITER ',' CSV;", tableName, fileName ))
+		writeCOPYSQL(sql, "data.sql")
 	}
-	sort.Strings(keys)
-	
-	fmt.Println(fmt.Sprintf("\\COPY %s (" + strings.Join(keys, ", ") + ") FROM '%s' DELIMITER ',' CSV;", tableName, fileName ))
 	return nil
 }
 
+func processRecord(record *airtable.Record, currentTable *airtable.TableSchema, r []*a.RelationshipInfo, client *airtable.Client, b *airtable.Base) ([]string, error) {
+	var row []string
+	var keys []string
+	for k := range record.Fields {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	
+	for _, fieldName := range keys {
+		fieldValue := record.Fields[fieldName]
+		var appendBool = true
+		var valueToStore = fieldValue
+		var err = errors.New("")
+		// Si le duo fieldName
+		// Dans notre objet Base on a notre table courante et le field courant
+		currentField, _ := a.FindFieldByName(currentTable.Fields, fieldName)
+		if currentField.Type == "multipleRecordLinks" {
+			appendBool, valueToStore, err = processField(client, b, r, currentTable, currentField, fieldValue)
+			if err != nil {
+				return nil, err
+			}
+		}
+		// fmt.Println(currentField)
+		if appendBool {
+			row = append(row, fmt.Sprintf("%v", valueToStore))
+		}
+	}
 
-func writePreDataToCSV(lines []string, fileName string) error {
+	return row, nil
+
+}
+
+func processField(client *airtable.Client, b *airtable.Base, r []*a.RelationshipInfo, currentTable *airtable.TableSchema, currentField *airtable.Field, fieldValue interface{}) (bool, interface{}, error) {
+	appendBool := true
+	valueToStore := fieldValue
+
+	currentRelationIfFound, _ := a.FindRelationByTableIDAndFieldID(r, currentTable.ID, currentField.ID)
+	if currentRelationIfFound != nil {
+		if currentRelationIfFound.RelationType == "OneToMany" || currentRelationIfFound.RelationType == "ManyToMany" {
+			appendBool = false
+		} else if currentRelationIfFound.RelationType == "ManyToOne" {
+			fieldValueSlice, ok := fieldValue.([]interface{})
+			if !ok {
+				return false, nil, fmt.Errorf("fieldValue n'est pas un slice")
+			}
+			id, ok := fieldValueSlice[0].(string)
+			if !ok {
+				return false, nil, fmt.Errorf("cannot convert fieldValue to string")
+			}
+
+			var relatedTable = client.GetTable(b.ID, currentRelationIfFound.RelatedTable.Name)
+			currentRecord, err := relatedTable.GetRecord(id)
+			if err != nil {
+				return false, nil, err
+			}
+			
+			primaryField, _ := a.FindFieldByID(&currentRelationIfFound.RelatedTable, currentRelationIfFound.RelatedTable.PrimaryFieldID)
+
+			valueToStore = currentRecord.Fields[primaryField.Name]
+		}
+	}
+
+	return appendBool, valueToStore, nil
+}
+
+
+func writeSQL(lines []string, fileName string) error {
 	// Créer le fichier
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -115,6 +142,34 @@ func writePreDataToCSV(lines []string, fileName string) error {
 	}
 
 	return nil
+}
+
+func writeCOPYSQL(lines []string, fileName string) error {
+    // Ouvre le fichier en mode append, crée le fichier s'il n'existe pas, 
+    // et ouvre le fichier en mode écriture seulement
+    file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    // Parcourir chaque ligne et l'écrire dans le fichier
+    for _, str := range lines {
+        _, err := file.WriteString(str + "\n")
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
+
+func deleteFile(filename string) {
+	err := os.Remove(filename)
+	if err != nil {
+		// Si une erreur se produit lors de la suppression du fichier
+		fmt.Println("Erreur lors de la suppression du fichier", err)
+	}
 }
 
 func main() {
@@ -174,9 +229,10 @@ func main() {
 					lines = append(lines, line)
 				}
 				
-				writePreDataToCSV(lines, "pre-data.sql")
+				writeSQL(lines, "pre-data.sql")
 			}
 			if *data {
+				deleteFile("data.sql")
 				for _, table := range postgresBase.Tables {
 					err := exportTableToCSV(client, base, postgresBase.RelationshipInfos, table.Name)
 					if err != nil {
@@ -185,7 +241,7 @@ func main() {
 				}
 			}
 			if *postData {
-				writePreDataToCSV(postgresBase.PostDataSQL(), "post-data.sql")
+				writeSQL(postgresBase.PostDataSQL(), "post-data.sql")
 			}
 		}
 	}
